798904f89f6d99ba4aad38560e2feb42407b8ae7
harismawan <achmadfauziharismawan@gmail.com>
2017-10-08 15:48:38 +0700

    change single Thread implementation with Multithreading

--- app/src/main/java/com/harismawan/rxandroidexample/MainActivity.kt
+++ app/src/main/java/com/harismawan/rxandroidexample/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.harismawan.rxandroidexample.ui
+package com.harismawan.rxandroidexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.harismawan.rxandroidexample.R
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -14,45 +13,59 @@ import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
+    //create Thread scheduler based on number of processor
+    private val threadCount = Runtime.getRuntime().availableProcessors()
+    private val threadPoolExecutor = Executors.newFixedThreadPool(threadCount)
+    private var scheduler = Schedulers.from(threadPoolExecutor)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //populate stringList data
         val stringList = ArrayList<String>()
-        for (i in 0..100) {
+        for (i in 0..1000) {
             stringList.add("Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet")
         }
 
         print("initializing\n")
-        val result = ArrayList<List<String>>()
+        val tasks = ArrayList<Observable<List<String>>>()
         execute.setOnClickListener {
-            execute(stringList, result)
+            execute(stringList, tasks)
         }
     }
 
-    private fun execute(stringList: ArrayList<String>, result: ArrayList<List<String>>) {
-        result.clear()
+    private fun execute(stringList: ArrayList<String>, tasks: ArrayList<Observable<List<String>>>) {
+        tasks.clear()
 
         print("process started\n")
         val start = System.currentTimeMillis()
 
-        stringList.mapTo(result) { it.split(" ") }
+        //split stringList into List of words on background Threads
+        stringList.mapTo(tasks) {
+            Observable.just(it).subscribeOn(scheduler).map {
+                it.split(" ")
+            }
+        }
+
+        //wait all Thread to finish and collect the result
+        val result = ArrayList<List<String>>()
+        result += Observable.merge(tasks).blockingIterable()
 
         print("process ended\n")
         val end = System.currentTimeMillis()
-        val elapsed = end - start
 
-        print("elapsed time: " + elapsed.toString())
+        val elapsed = end - start
+        print("elapsed time: " + elapsed.toString() + "\n")
     }
 
+    //Observable for printing output
     private fun print(text: String) {
-        val threadCount = Runtime.getRuntime().availableProcessors()
-        val threadPoolExecutor = Executors.newFixedThreadPool(threadCount)
-        val scheduler = Schedulers.from(threadPoolExecutor)
         Observable.just(text).subscribeOn(scheduler).
                 observeOn(AndroidSchedulers.mainThread()).subscribe(OutputObserver(output))
     }
 
+    //Observer for printing new event to output TextView
     class OutputObserver(private var output: TextView) : Observer<String> {
 
         override fun onSubscribe(d: Disposable?) {
@@ -60,6 +73,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onNext(value: String?) {
+            //print to output TextView
             output.append(value)
         }
 
